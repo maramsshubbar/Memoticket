60949bcfab53fd47ea70861f290cfeee985b5bb6
Maram Shubbar <maram20052@gmail.com>
2026-08-23 15:56:12 +0300

    add collection creation

--- controllers/collectionsCtrl.js
+++ controllers/collectionsCtrl.js
@@ -16,6 +16,14 @@ const newCollection = (req,res)=> {
     res.render('collections/new.ejs');
 };
 
+
+const create = async (req,res) => {
+    req.body.user = req.session.user._id;
+
+    await Collection.create(req.body);
+    res.redirect('/collections');
+};
+
 module.exports = {
     index,
     new:newCollection,
