092e766ae42fd1622cc2beb8ed304985389c7a92
Maram Shubbar <maram20052@gmail.com>
2026-08-26 01:45:19 +0300

    Update page routes

--- routes/pagesRouter.js
+++ routes/pagesRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const pagesCtrl = require('../controllers/pagesCtrl');
 
 const router = express.Router();
 
 router.get('/', pagesCtrl.home);
+router.get('/about', pagesCtrl.about);
+router.get('/memories', pagesCtrl.memories);
 
 module.exports = router;
