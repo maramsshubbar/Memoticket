e4d3347a419e171e6f01565d21efbb3ad55748f0
Maram Shubbar <maram20052@gmail.com>
2026-08-26 01:45:03 +0300

    Add page controller functionality

--- controllers/pagesCtrl.js
+++ controllers/pagesCtrl.js
@@ -2,6 +2,27 @@ const home = async (req, res) => {
   res.render('index.ejs');
 };
 
+
+const about = (req, res) => {
+  res.render('about');
+};
+
+
+const Memory = require('../models/memory');
+
+const memories = async (req, res) => {
+  const allMemories = await Memory.find({
+    user: req.session.user._id,
+  }).sort({ date: -1 });
+
+  res.render('memories/index.ejs', {
+    memories: allMemories,
+  });
+};
+
+
 module.exports = {
   home,
+  about,
+  memories,
 };
